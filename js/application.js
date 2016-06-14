bbb5a01d8c2e9d367ee65f9e1df99e5fe48f27d7
Jakub Rak <theaeroson@gmail.com>
2016-06-15 00:17:16 +0200

    no message

--- js/application.js
+++ js/application.js
@@ -1,5 +1,8 @@
-ZeroClipboard.config(
-{
-    swfPath: 'http://am2.taw.net/assets/js/zeroclipboard.swf'
-});
-var client = new ZeroClipboard($(".copy-button"));
+//$(document).ready(function(){
+	ZeroClipboard.config(
+	{
+	    swfPath: 'https://cdnjs.cloudflare.com/ajax/libs/zeroclipboard/2.2.0/ZeroClipboard.swf',
+	    forceHandCursor: true,
+	});
+	var client = new ZeroClipboard($(".copy-button"));
+//});
